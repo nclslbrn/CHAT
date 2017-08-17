b3c50c55d503a38c5eba037305a5f30c5bf803be
Nicolas Lebrun <lnmy@hotmail.fr>
2017-08-17 12:08:16 +0200

    Update launch_chrome.sh

--- tinker_board_bash_script/launch_chrome.sh
+++ tinker_board_bash_script/launch_chrome.sh
@@ -2,7 +2,7 @@
 sleep 20s 
 export DISPLAY=:0.0
 open -a "Google Chrome" index.html
-chromium-browser --app=http://example.com --start-fullscreen
+chromium-browser --kiosk --app=http://example.com --start-fullscreen
 sleep 5s
 xdotool mousemove 0 1080
 xdotool search --name ".*Mozilla Firefox" key "F11"
